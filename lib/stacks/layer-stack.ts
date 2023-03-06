e35c70a94cbc7f5909fd851f5cf214f4e009750c
Ville Häkli <ville@vhx.fi>
2023-03-06 21:21:36 +0200

    Testing

--- lib/stacks/layer-stack.ts
+++ lib/stacks/layer-stack.ts
@@ -1,15 +1,14 @@
 import { Stack, StackProps } from "aws-cdk-lib";
 import { Construct } from "constructs";
-import { Architecture, Code, LayerVersion, Runtime } from "aws-cdk-lib/aws-lambda";
+import { Code, LayerVersion, Runtime } from "aws-cdk-lib/aws-lambda";
 
 export class LayerStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    new LayerVersion(this, "TestLayer", {
+    new LayerVersion(this, "xray-layer", {
       compatibleRuntimes: [Runtime.NODEJS_18_X],
-      code: Code.fromAsset("./dist/aws-xray-sdk"),
-      compatibleArchitectures: [Architecture.X86_64],
+      code: Code.fromAsset("./dist/xray"),
     });
   }
 }
