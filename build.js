b8ae50af3d964bac4cbf0a348c2aa4134f534270
Ville Häkli <ville.hakli@vaisala.com>
2023-03-08 15:35:02 +0200

    Stuff and things and such

--- build.js
+++ build.js
@@ -2,25 +2,33 @@ const esbuild = require("esbuild");
 const path = require("path");
 
 (async () => {
-  const arg = process.argv[2];
+  const project = process.argv[2];
 
-  if (!arg) {
-    throw new Error("No argument provided");
+  if (!project) {
+    throw new Error("No project name provided");
   }
 
-  const layerDir = path.join(__dirname, `./layers/${arg}`);
-  const layer = path.join(layerDir, `${arg}.ts`);
+  const layerDir = path.join(__dirname, `./layers/${project}`);
+  const layer = path.join(layerDir, `${project}.ts`);
   const outfile = path.join(
     layerDir,
-    "nodejs/node_modules/@villehx",
-    `${arg}.js`
+    "dist/nodejs/node_modules/@villehx",
+    `${project}.js`
   );
 
-  await esbuild.build({
-    platform: "node",
-    target: "node18",
-    bundle: true,
-    entryPoints: [layer],
-    outfile,
-  });
+  console.log(`building "${project}" layer`);
+  await esbuild
+    .build({
+      platform: "node",
+      target: "node18",
+      bundle: true,
+      entryPoints: [layer],
+      outfile,
+    })
+    .then(() => {
+      console.log("build complete");
+    })
+    .catch(() => {
+      console.error("build failed");
+    });
 })();
